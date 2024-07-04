2780b536b652b60640d981a6c75f867ca5c7731f
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2024-07-04 20:38:14 +0700

    belajar setup docker

---
+++ src/main/java/com/ahmad/belajar/docker/BelajarDockerApplication.java
@@ -0,0 +1,13 @@
+package com.ahmad.belajar.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ahmad/belajar/docker/BelajarDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmad.belajar.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
